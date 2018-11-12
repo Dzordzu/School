fd2e46179b3d9250ede21316f537c8d7f80e37e8
Dzordzu <tomekdur@wp.pl>
2018-11-12 15:15:02 +0100

    Removed unused var

--- lib/CustomAlgorithms/Heuristic/GeneticKnapsack/GeneticAlgorithm.h
+++ lib/CustomAlgorithms/Heuristic/GeneticKnapsack/GeneticAlgorithm.h
@@ -14,9 +14,7 @@ class GeneticAlgorithm {
     GETSET(float, CrossingProbability);
     GETSET(float, MutationProbability);
     GETSET(uint16_t, GenerationsAmount);
-    GETSET(uint32_t, KnapsackSize);
     GETSET(uint16_t, Iterations);
-
 
 public:
     void run();
