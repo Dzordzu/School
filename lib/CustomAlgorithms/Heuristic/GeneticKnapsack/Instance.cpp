9a8ebab37ab01d9b7eba61ec3772ac2c883820dc
Dzordzu <tomekdur@wp.pl>
2018-11-12 14:01:41 +0100

    Added instance class

---
+++ lib/CustomAlgorithms/Heuristic/GeneticKnapsack/Instance.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dzordzu on 12.11.18.
+//
+
+#include "Instance.h"
---
+++ lib/CustomAlgorithms/Heuristic/GeneticKnapsack/Instance.h
@@ -0,0 +1,14 @@
+//
+// Created by dzordzu on 12.11.18.
+//
+
+#ifndef SCHOOL_INSTANCE_H
+#define SCHOOL_INSTANCE_H
+
+
+class Instance {
+    
+};
+
+
+#endif //SCHOOL_INSTANCE_H
