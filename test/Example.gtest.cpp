f489e6eb1a1649eeec9d3e34f0fb3b3374026a91
Dzordzu <tomekdur@wp.pl>
2018-11-17 18:10:51 +0100

    Removed workspace.xml in git + added example gtest + modified cmakelist

---
+++ test/Example.gtest.cpp
@@ -0,0 +1,21 @@
+//
+// Created by dzordzu on 17.11.18.
+//
+
+#include "../lib/gtest/gtest.h"
+
+TEST(SquareRootTest, PositiveNos) {
+    EXPECT_EQ (18.0, 18.0);
+    EXPECT_EQ (13.0, 13.0);
+    EXPECT_EQ (18.3, 18.3);
+}
+
+TEST (SquareRootTest, ZeroAndNegativeNos) {
+    ASSERT_EQ (0.0, 0.1);
+    ASSERT_EQ (-1, -22.0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
