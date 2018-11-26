8e17cacb03d3715d25b642774f59f5a67599bbcf
Dzordzu <tomekdur@wp.pl>
2018-11-26 23:30:31 +0100

    Changed API for Fitness interface

--- project/GeneticAlgorithm/Fitness.h
+++ project/GeneticAlgorithm/Fitness.h
@@ -5,9 +5,27 @@
 #ifndef SCHOOL_FITNESS_H
 #define SCHOOL_FITNESS_H
 
+#include <exception>
+#include <typeinfo>
+#include <string>
+
+class Fitness;
+
+class FitnessException : std::exception {
+    std::string message;
+public:
+    FitnessException(Fitness *type) {}
+    const char* what() const noexcept {}
+};
+
 
 class Fitness {
-
+public:
+    virtual bool operator==(Fitness &f);
+    virtual bool operator<(Fitness &f);
+    virtual bool operator<=(Fitness &f);
+    virtual bool operator>(Fitness &f);
+    virtual bool operator>=(Fitness &f);
 };
 
 
