d85734f5d1e0e0c946ef230a53c03cdb67b6cb34
Dzordzu <tomekdur@wp.pl>
2018-11-27 00:58:28 +0100

    Added tests

--- project/GeneticAlgorithm/Test/Test.cpp
+++ project/GeneticAlgorithm/Test/Test.cpp
@@ -5,13 +5,37 @@
 #include <gtest/gtest.h>
 #include <Fitness.h>
 
-class FitnessNonOverloadedExample : public Fitness {};
+class FitnessNonOverloaded : public Fitness {};
+
+class FitnessOverloaded : public Fitness {
+    int value;
+public:
+    explicit FitnessOverloaded(int value) {this->value = value; }
+    bool operator==(const FitnessOverloaded &f) { return this->value == f.value; }
+    bool operator>(const FitnessOverloaded &f) { return this->value > f.value; }
+    bool operator<(const FitnessOverloaded &f) { return this->value < f.value; }
+    bool operator>=(const FitnessOverloaded &f) { return *this>f || *this == f; }
+    bool operator<=(const FitnessOverloaded &f) { return *this<f || *this == f; }
+};
 
 namespace {
 
     TEST(FitnessTest, NonOverloaded) {
-        FitnessNonOverloadedExample f1, f2;
-        EXPECT_THROW(f1 == f2, FitnessException);
+        FitnessNonOverloaded f1, f2;
+//        EXPECT_THROW(f1 == f2, FitnessException);
+//        EXPECT_THROW(f1 <= f2, FitnessException);
+//        EXPECT_THROW(f1 >= f2, FitnessException);
+//        EXPECT_THROW(f1 < f2, FitnessException);
+//        EXPECT_THROW(f1 > f2, FitnessException);
+    }
+
+    TEST(FitnessTest, Overloaded) {
+        FitnessOverloaded f1(1), f2(2), f3(1);
+        EXPECT_TRUE(f1 == f3);
+        EXPECT_TRUE(f2 > f3);
+        EXPECT_TRUE(f3 < f2);
+        EXPECT_TRUE(f1 <= f2);
+        EXPECT_TRUE(f1 >= f3);
     }
 }
 
