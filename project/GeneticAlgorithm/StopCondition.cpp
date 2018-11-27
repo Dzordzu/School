232b6535aef7b1eb30f2f1466a2e79fba8e59aad
Dzordzu <tomekdur@wp.pl>
2018-11-27 12:50:26 +0100

    Added StopCondition implementation

--- project/GeneticAlgorithm/StopCondition.cpp
+++ project/GeneticAlgorithm/StopCondition.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "StopCondition.h"
+
+StopCondition::StopCondition(StopCondition::Type type, uint64_t value) {
+    this->type = type;
+    this->valueInt = value;
+}
+
+StopCondition::StopCondition(StopCondition::Type type, Fitness valueFitness) {
+    this->type = type;
+    this->valueFitness = valueFitness;
+}
+
+StopCondition::Type StopCondition::getType() {
+    return this->type;
+}
+
+bool StopCondition::isFulfilled(Fitness &other) {
+    return this->valueFitness > other;
+}
+
+bool StopCondition::isFulfilled(uint32_t &other) {
+    return this->valueInt > other;
+}
